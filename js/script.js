26e0af5cc6ad692c3a206c4cc07dfd75155b8949
AbdelrahmanMahmoud74 <133714220+AbdelrahmanMahmoud74@users.noreply.github.com>
2023-11-14 17:10:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,79 @@
+let app=document.getElementById('app');
+// app.innerHTML=` <nav> <div class="logo">
+//  <i class="fas fa-fire"></i> <h4>Logo</h4></div> <ul class="nav-items"><button class="home" ><a href="#">Home</a></button> <li class="nav-item"><a href="#" class="nav-link by">About</a></li> <li class="nav-item"><a href="#" class="nav-link">Products</a></li> <li class="nav-item"><a href="#" class="nav-link">Services <i class="fas fa-sort-down"></i></a></li></ul> <input class="input" type="search"><button class="btn">Search</button></div></nav> <div class="section-one"><h1 class="title-section-one">Dramatically Engage</h1><p class="p-section-one">Objectively innovate empowered manufactured products whereas parallel platforms.</p><button class="btn1 "><a href="#">Engage Now</a></button></div>  <div class="titles"><span class="head-title">Superior Collaboration</span><span class="head-title1">Visualize Quality</span></div><p class="p-head">Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing.</p><p class="p-head1">Holistically pontificate installed base portals after maintainable products.</p><div class="section-1"><h2 class="section-one-title">700x300</h2><h3 class="h3-title"><a href="#">Efficiently Unleash </a></h3><p class="p-title">Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p></div><div class="section-two"><h2 class="section-one-title">700x300</h2><h3 class="h3-title"><a href="#">Completely Synergize </a></h3><p class="p-title">Dramatically maintain clicks-and-mortar solutions without functional solutions. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p></div><div class="section-three"> <h2 class="section-one-title">700x300</h2><h3 class="h3-title"><a href="#">Dynamically Procrastinate </a></h3><p class="p-title">Professionally cultivate one-to-one customer service with robust ideas. Completely synergize resource taxing relationships via premier niche markets. Dynamically innovate resource-leveling customer service for state of the art customer service.</p></div>';
+app.innerHTML=`<nav> <div class="logo">
+ <i class="fas fa-fire"></i>
+  <h4>Logo</h4>
+  </div> 
+  <ul class="nav-items"><button class="home" ><a href="#">Home</a></button>
+   <li class="nav-item"><a href="#" class="nav-link by">About</a></li> 
+   <li class="nav-item"><a href="#" class="nav-link">Products</a></li>
+    <li class="nav-item active"><a href="#" class="nav-link">Services <i class="fas fa-sort-down"></i></a>
+    <div class="sub-menu-1">
+    <ul>
+    <li class="non"><a href="#">Engage</a</li>
+    <li class="non"><a href="#">Pontificate</a</li>
+    <li class="non"><a href="#">Synergize</a</li>
+    </ul>
+    </div>
+    </li></ul> 
+    <input class="input" type="search"><button class="btn">Search</button>
+    <i class="fas fa-bars" id="icon"></i>
+    </div>
+    </nav>
+     <div class="section-one">
+     <h1 class="title-section-one">Dramatically Engage</h1>
+     <p class="p-section-one">Objectively innovate empowered manufactured products whereas parallel platforms.</p>
+     <button class="btn1 "><a href="#">Engage Now</a></button>
+     </div> 
+      <div class="titles">
+      <span class="head-title">Superior Collaboration</span>
+      <span class="head-title1">Visualize Quality</span>
+      </div>
+      <p class="p-head">Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing.</p>
+      <p class="p-head1">Holistically pontificate installed base portals after maintainable products.</p>
+      <div class="section-1">
+      <h2 class="section-one-title">700x300</h2>
+      <h3 class="h3-title"><a href="#">Efficiently Unleash </a></h3>
+      <p class="p-title">Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+      </div>
+      <div class="section-two">
+      <h2 class="section-one-title">700x300</h2>
+      <h3 class="h3-title"><a href="#">Completely Synergize </a></h3>
+      <p class="p-title">Dramatically maintain clicks-and-mortar solutions without functional solutions. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
+      </div>
+      <div class="section-three"> 
+      <h2 class="section-one-title">700x300</h2>
+      <h3 class="h3-title"><a href="#">Dynamically Procrastinate </a></h3>
+      <p class="p-title">Professionally cultivate one-to-one customer service with robust ideas. Completely synergize resource taxing relationships via premier niche markets. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
+      </div>
+
+      <div class="section-four">
+      <br>
+      <br>
+      <br><br><br>
+      <div class="bren">
+          <h2 class="h2-section">100x100</h2>
+      </div>
+      <h3 class="h3-section">Dynamically Procrastinate </h3>
+  <p class="p-title1">Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+      <button class="btn2"><a href="#">Procrastinate</a></button>
+  <div class="bren1">
+          <h2 class="h2-section">100x100</h2>
+      </div>
+      <h3 class="h31-section">Efficiently Unleash </h3>
+  <p class="p1-title1">Dramatically maintain clicks-and-mortar solutions without functional solutions. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
+  <button class="btn3"><a href="#">unleash</a></button>
+  <div class="bren2">
+          <h2 class="h2-section">100x100</h2>
+      </div>
+      <h3 class="h311-section">Completely Synergize </h3>
+      <p class="p11-title1">Professionally cultivate one-to-one customer service with robust ideas. Completely synergize resource taxing relationships via premier niche markets. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
+      <button class="btn4"><a href="#">Synergize</a></button>
+  </div>
+  <div class="basha">
+      <span class="terms"><a href="#">Terms & Conditions |</a></span>
+      <span class="terms"><a href="#">Privacy Policy |</a></span>
+      <span class="terms"><a href="#">Contact</a></span>
+      <p class="terms1">Copyright © Example.com 2015</p>`;
+      
